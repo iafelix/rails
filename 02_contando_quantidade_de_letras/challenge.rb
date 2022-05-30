4343f9f76554d4d004d198ff6a7773256db17d64
Nelson Rodrigues Ferreira Junior <nelsonrf@br.ibm.com>
2022-05-30 17:40:32 -0300

    Execercise 2

---
+++ 02_contando_quantidade_de_letras/challenge.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+def prompt_for_string
+  print 'What is the input string? '
+  gets.strip
+end
+
+input_string = prompt_for_string
+while input_string.length.zero?
+  puts 'The input string is required.'
+  input_string = prompt_for_string
+end
+
+puts "#{input_string} has #{input_string.length} characters."
---
+++ 02_contando_quantidade_de_letras/exercise.rb
@@ -0,0 +1,5 @@
+#!/usr/bin/env ruby
+
+print 'What is the input string? '
+input_string = gets.strip
+puts "#{input_string} has #{input_string.length} characters."
